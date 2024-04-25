0432c5494e3525e40c4aa53c04cd9574eb8559a5
CesarMartinez23 <oliver-cesar@hotmail.com>
2024-04-24 18:51:03 -0600

    Relacionar los modelos de tareas y proyectos

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,16 +1,22 @@
-import mongosse, { Schema, Document } from "mongoose";
+import mongosse, { Schema, Document, PopulatedDoc, Types } from "mongoose";
+import { ITask } from "./Task";
 
 export interface IProject extends Document {
   projectName: string;
   clientName: string;
   description: string;
+  tasks: PopulatedDoc<ITask & Document>[];
 }
 
-const ProjectSchema: Schema = new Schema({
-  projectName: { type: String, required: true, trim: true },
-  clientName: { type: String, required: true, trim: true },
-  description: { type: String, required: true, trim: true },
-});
+const ProjectSchema: Schema = new Schema(
+  {
+    projectName: { type: String, required: true, trim: true },
+    clientName: { type: String, required: true, trim: true },
+    description: { type: String, required: true, trim: true },
+    tasks: [{ type: Types.ObjectId, ref: "Task" }],
+  },
+  { timestamps: true }
+);
 
 const Project = mongosse.model<IProject>("Project", ProjectSchema);
 export default Project;
